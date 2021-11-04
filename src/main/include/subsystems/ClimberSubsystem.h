de2bf6f73ef11e963d467f8145c4071cd05e04f7
5690 <5690Programmers@gmail.com>
2021-11-04 17:07:17 -0500

    Added some comments
    it's still broken tho sorry
    
    look for m_climber in RobotContainer.h
    if you comment that out it builds but idk if it fixes the problem

--- src/main/include/subsystems/ClimberSubsystem.h
+++ src/main/include/subsystems/ClimberSubsystem.h
@@ -1,3 +1,10 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
 #pragma once
 
 #include <ctre/Phoenix.h>
@@ -5,10 +12,17 @@
 
 #include <frc2/command/SubsystemBase.h>
 
+#include "Constants.h"
+
 class ClimberSubsystem : public frc2::SubsystemBase {
  public: 
   ClimberSubsystem();
 
+// Subsystem methods go here.
+
+  /**
+   * Will be called periodically whenever the CommandScheduler runs.
+   */
   void Periodic() override;
 
   void ClimberUp();
@@ -20,7 +34,8 @@ class ClimberSubsystem : public frc2::SubsystemBase {
   void ClimberStop();
 
  private:
-
+  // Components (e.g. motor controllers and sensors) should generally be
+  // declared private and exposed only through public methods.
   WPI_VictorSPX Climber{7};   //Climber lift
   WPI_VictorSPX Winch{11};    //Climber
 
